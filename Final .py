a86e6f632dd41fe004e1c1d67c1e72b4189e2994
Sofia Ni <56848161+sofianizh@users.noreply.github.com>
2020-04-20 20:44:36 -0400

    Final Project

---
+++ Final .py
@@ -0,0 +1,18 @@
+# Write your code here :-)
+from microbit import *
+
+#sense the light
+while True:
+    if button_a.was_pressed(): Light Sensor
+        Lightvalue = pin0.read_analog()
+        if Lightvalue > 400:
+            display.show(Image.HEART) #test
+            sleep(100)
+            pin1.write_digital(1) #Flash LED
+            sleep(100)
+        else:
+            display.show(Image.SAD) #test
+            pin1.write_digital(0) #Turn off the LED
+            sleep(100)
+    else: #Ultrasonic
+        if button_b.was_pressed():
